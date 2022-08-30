c0263b41cf4635978d098d0b8f8d07129bd526e3
shravan <shravan.madipelly@gmail.com>
2022-08-30 10:24:36 -0700

    added demo POC

---
+++ Demo_POC/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  application:
+  {
+    name: 'angular-ssr',
+    version: 'Angular 14.1.3',
+    bootstrap: 'Bootstrap 5.2.0',
+    fontawesome: 'Font Awesome 6.1.2',
+  }
+};
---
+++ Demo_POC/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  application:
+  {
+    name: 'angular-ssr',
+    version: 'Angular 14.1.3',
+    bootstrap: 'Bootstrap 5.2.0',
+    fontawesome: 'Font Awesome 6.1.2',
+  }
+};
